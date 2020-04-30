06069c961caa9cc389aa3c06121c7da50f185cdf
andrea.lombardi <v.andrea.lombardi@red.virgin.com>
2020-04-30 11:05:40 +0100

    adjust expectation

--- src/product/product.pact.test.js
+++ src/product/product.pact.test.js
@@ -38,12 +38,12 @@ describe("Pact Verification", () => {
     }
 
     const stateHandlers = {
-      "a product with ID 10 exists": () => {
+      "product with ID 10 exists": () => {
         controller.repository.products = new Map([
           ["10", new Product("10", "CREDIT_CARD", "28 Degrees", "v1")]
         ]);
       },
-      "a product with ID 11 does not exist": () => {
+      "product with ID 11 does not exist": () => {
         controller.repository.products = new Map();
       }
     }
